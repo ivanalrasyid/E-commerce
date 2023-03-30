159af8b3263341292c0ffaa90dd324e87aa652c1
ivanalrasyid <ivanalrasyid11@gmail.com>
2023-03-30 22:44:09 +0700

    Slicing E-commerce use Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        secondary2: '#DB4444',
+        text2: '#FAFAFA'
+      }
+    },
+  },
+  plugins: [],
+}
